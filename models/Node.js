98079f25faf82bd5640b6cbe241fba18685bca70
Saad Ahmed Khan <saadahmedkhan2002sak@gmail.com>
2025-04-11 16:57:41 +0500

    added output schema

--- models/Node.js
+++ models/Node.js
@@ -4,37 +4,43 @@ const schema = Schema;
 
 
 const ConfigurationSchema = new Schema({
-    queryType: { type: String },
-    type: { type: String },
-    outputColums: [String],
-    operator: { type: String },
-  }, { _id: false });
-  
-  const EdgeSchema = new Schema({
-    id: {type: String},
-    type: {type: String}
-  }, {_id: false});
+  queryType: { type: String },
+  type: { type: String },
+  outputColums: [String],
+  operator: { type: String },
+}, { _id: false });
 
+const EdgeSchema = new Schema({
+  id: { type: String },
+  type: { type: String }
+}, { _id: false });
+
+const outputSchema = Schema({
+  id: {type: String},
+  outputColums: [String],
+  edges: [EdgeSchema]
+})
 
 const NodeSchema = new Schema()
-    NodeSchema.add({
-    x: {type: Number},
-    y: {type: Number},
-    id: {type: String},
-    name: {type: String},
-    type: {type: String},
-    nodeType: {type: String},
-    edges: [EdgeSchema],
-    edgesFrom: [EdgeSchema],
-    configuration: ConfigurationSchema, 
-    children: [NodeSchema]
+NodeSchema.add({
+  x: { type: Number },
+  y: { type: Number },
+  id: { type: String },
+  name: { type: String },
+  type: { type: String },
+  nodeType: { type: String },
+  edges: [EdgeSchema],
+  edgesFrom: [EdgeSchema],
+  configuration: ConfigurationSchema,
+  children: [NodeSchema],
+  output: outputSchema
+
 });
 
 
 const ApiBuilderSchema = new Schema({
-    apiConfig: {type: schema.Types.ObjectId, ref: "ApiConfigs", required: true},
-    valid: {type: Boolean, default: false},
-    nodes: [NodeSchema]
+  apiConfig: { type: schema.Types.ObjectId, ref: "ApiConfigs", required: true },
+  nodes: [NodeSchema]
 })
 
 const ApiBuilder = model('ApiBuilder', ApiBuilderSchema);
